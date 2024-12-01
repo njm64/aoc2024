e7a7c60ee97453d2f2b16d43ac4aec4b678e2bb1
Nick Maher <nick.maher@gmail.com>
2024-12-01 15:29:11 +1100

    Initial commit

---
+++ haskell/day1.hs
@@ -0,0 +1,10 @@
+module Day1 where
+import Util
+import qualified Aoc
+
+parse = id
+
+part1 ds = 1
+part2 ds = 1
+run = Aoc.run 1 id part1 part2
+
---
+++ haskell/main.hs
@@ -0,0 +1,25 @@
+import System.Environment
+import Text.Read
+import Data.Maybe
+import qualified Day1 
+
+runners = [
+  Day1.run]
+  
+parseInt :: String -> Maybe Int
+parseInt s = case reads s of
+  [(n, "")] -> Just n
+  _ -> Nothing
+  
+runnerForDay :: Int -> Maybe (IO ())
+runnerForDay n = listToMaybe $ drop (n - 1) runners
+  
+main :: IO ()
+main = do
+  args <- getArgs
+  if null args then
+    sequence_ runners
+  else 
+    case parseInt (head args) >>= runnerForDay of
+      Just r -> r
+      _ -> putStrLn "Usage: aoc <day>"
