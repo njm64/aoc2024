af0c88131ecc8f61c63a1f56fe77fa4bdba72f76
Nick Maher <nick.maher@gmail.com>
2024-12-02 20:44:56 +1100

    Fixed indent width

--- haskell/day1.hs
+++ haskell/day1.hs
@@ -1,16 +1,15 @@
 module Day1 where
-import Util
+import Aoc qualified
 import Data.List
-import qualified Aoc
+import Util
 
 parse :: [String] -> [[Int]]
 parse = transpose . map (map read . words)
-    
-part1 [list1, list2] =
-    sum [abs (a - b) | (a,b) <- zip (sort list1) (sort list2)]
 
-part2 [list1, list2] = 
-    sum [i | i <- list1, j <- list2, i == j]
+part1 [list1, list2] =
+  sum [abs (a - b) | (a, b) <- zip (sort list1) (sort list2)]
+
+part2 [list1, list2] =
+  sum [i | i <- list1, j <- list2, i == j]
 
 run = Aoc.run 1 parse part1 part2
-
