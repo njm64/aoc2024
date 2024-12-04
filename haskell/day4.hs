96ec2b92ea9a04b607091e3a51d8f2d4dbce0477
Nick Maher <nick.maher@gmail.com>
2024-12-04 21:49:04 +1100

    Day 4

---
+++ haskell/day4.hs
@@ -0,0 +1,36 @@
+module Day4 where
+import Aoc qualified
+import Data.Array
+
+type Coord = (Int, Int)
+type WordMap = Array Coord Char
+
+parse :: [String] -> WordMap
+parse lines =
+  let width = length (head lines) in
+  let height = length lines in
+  let coords = [(x, y) | y <- [1..height], x <- [1..width]] in
+  let chars = concat lines in
+  array ((1,1), (width, height)) (zip coords chars)
+
+countAtPoint :: WordMap -> Coord -> Int
+countAtPoint m (x,y) = length $ filter checkDir dirs
+  where dirs = [(dx,dy) | dx <- [-1..1], dy <- [-1..1]]
+        checkDir (dx,dy) = [m ! p | i <- [0..3], 
+                            let p = (x + dx * i, y + dy * i), 
+                            inRange (bounds m) p ] == "XMAS"
+
+isCross :: WordMap -> Coord -> Bool
+isCross m (x,y) = 
+  let deltas = [(-1,-1), (1,-1), (0,0), (-1,1), (1,1)] in
+  let w = [m ! (x + dx, y + dy) | (dx,dy) <- deltas] in
+  elem w ["MSAMS", "SMASM", "SSAMM", "MMASS"]
+
+part1 m = sum . map (countAtPoint m) $ indices m
+
+part2 m = 
+  let (w,h) = snd $ bounds m in
+  length [1 | x <- [2..w-1], y <- [2..h-1], isCross m (x,y)]
+
+run = Aoc.run 4 parse part1 part2
+
--- haskell/main.hs
+++ haskell/main.hs
@@ -4,9 +4,10 @@ import Data.Maybe
 import qualified Day1 
 import qualified Day2 
 import qualified Day3
+import qualified Day4
 
 runners = [
-  Day1.run, Day2.run, Day3.run]
+  Day1.run, Day2.run, Day3.run, Day4.run]
   
 parseInt :: String -> Maybe Int
 parseInt s = case reads s of
