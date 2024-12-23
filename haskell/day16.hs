b5e803bda10b07678094fd80caf3228d5a113b63
Nick Maher <nick.maher@gmail.com>
2024-12-23 19:52:24 +1100

    Day 16 part 2

--- haskell/day16.hs
+++ haskell/day16.hs
@@ -2,7 +2,9 @@ module Day16 where
 import Aoc qualified
 import Data.List
 import Data.Array
+import Data.Maybe
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import qualified Heap
 import Util
 
@@ -11,7 +13,10 @@ type Map = Array Pos Char
 data Dir = North | East | South | West deriving (Show, Enum, Eq, Ord)
 type State = (Pos, Dir)
 type Cost = Int
-type CostMap = Map.Map State Int
+type CostMap = Map.Map State (Int, [State])
+
+allDirs :: [Dir]
+allDirs = [North, East, South, West]
 
 parse :: [String] -> Map
 parse lines =
@@ -20,8 +25,8 @@ parse lines =
   let coords = [(x,y) | y <- [1..height], x <- [1..width]] in
   array ((1,1), (width, height)) (zip coords (concat lines))
 
-findStartPos :: Map -> Pos
-findStartPos m = head [p | (p, c) <- assocs m, c == 'S']
+findPos :: Map -> Char -> Pos
+findPos m c = head [p | (p, c') <- assocs m, c == c']
 
 turnLeft :: Dir -> Dir
 turnLeft North = West
@@ -43,31 +48,72 @@ neighbours (c, (p, d)) =
    (c + 1000, (p, turnLeft d)),
    (c + 1000, (p, turnRight d))]
 
-search :: Map -> State -> Cost
-search m initialState =
+getCost :: CostMap -> State -> Cost
+getCost m state =
+  case Map.lookup state m of
+    Just (existingCost, _) -> existingCost
+    Nothing -> maxBound
+
+getParents :: CostMap -> State -> [State]
+getParents m state = 
+  case Map.lookup state m of
+    Just (_, existingParents) -> existingParents
+    Nothing -> []
+
+updateMap :: State -> CostMap -> (Cost, State) -> CostMap
+updateMap parentState m (cost, state) =
+  let existingCost = getCost m state in
+  let existingParents = getParents m state in
+  if cost > existingCost 
+     then m
+     else if cost == existingCost
+     then Map.insert state (cost, nub (parentState:existingParents)) m
+     else Map.insert state (cost, [parentState]) m
+  
+buildCostMap :: Map -> State -> CostMap
+buildCostMap m initialState =
   go (Heap.push Heap.empty 0 initialState) Map.empty where
     go heap costMap =
       case Heap.top heap of
-        Nothing -> error "Not found"
+        Nothing -> costMap
         Just (cost, state) ->
-          if isGoal state 
-             then cost
-             else let items = [i | i <- neighbours (cost, state),
-                                   isValidMove (snd i),
-                                   checkCost costMap i]
-                      heap' = foldl' updateHeap (Heap.pop heap) items
-                      costMap' = foldl' updateMap costMap items
-                  in go heap' costMap'
+         let items = [(c,s) | (c,s) <- neighbours (cost, state),
+                              isValidMove s, c <= getCost costMap s]
+             heap' = foldl' updateHeap (Heap.pop heap) items
+             costMap' = foldl' (updateMap state) costMap items
+           in go heap' costMap'
     updateHeap h (cost, state) = Heap.push h cost state
-    updateMap m (cost, state) = Map.insert state cost m
-    isGoal (pos, _) = m ! pos == 'E'
     isValidMove (pos, _) = m ! pos /= '#'
-    checkCost m (cost, state) = case Map.lookup state m of
-                                  Just existing -> cost < existing
-                                  Nothing -> True
                                         
+findAllPositions :: CostMap -> [State] -> [Pos]
+findAllPositions cm endStates =
+  go endStates Set.empty where
+  go [] set = nub . map fst $ Set.toList set
+  go (s:ss) set =
+    if Set.member s set
+       then go ss set
+       else let parents = snd . fromJust $ Map.lookup s cm in 
+              go (parents ++ ss) (Set.insert s set)
 
-part1 m = search m (findStartPos m, East)
-part2 = part1
+bestEndStates ::  CostMap -> Pos -> [State]
+bestEndStates cm end =
+  [state | (state, cost) <- statesWithCosts, cost == minCost]
+    where
+      endStates = [(end, d) | d <- allDirs, (end, d) `Map.member` cm]
+      statesWithCosts = [(s, costForState s) | s <- endStates]
+      costForState s = fst . fromJust $ Map.lookup s cm
+      minCost = minimum $ map snd statesWithCosts
+
+part1 m = 
+  let start = (findPos m 'S') in
+  let end = (findPos m 'E') in
+  let cm = buildCostMap m (start, East) in
+  minimum . map fst $ catMaybes [(Map.lookup (end, d) cm) | d <- allDirs]
+
+part2 m = 
+  let start = (findPos m 'S') in
+  let end = (findPos m 'E') in
+  let cm = buildCostMap m (start, East) in
+  length . findAllPositions cm $ bestEndStates cm end
 
 run = Aoc.run 16 parse part1 part2
